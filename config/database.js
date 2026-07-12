ef751f989470f3bd42d5e23114211dd1cc7ac64a
MoemenSuper <ouerghuimoemej@gmail.com>
2026-07-12 10:47:19 +0100

    Add initial task seed data

--- config/database.js
+++ config/database.js
@@ -3,10 +3,12 @@ const {open} = require("sqlite")
 const path = require("path");
 
 async function connectDatabase(){
+
     const db = await open({
         filename: path.join(__dirname,"..","database.sqlite"),
         driver: sqlite3.Database
     });
+
     await db.exec(`
         CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,6 +18,22 @@ async function connectDatabase(){
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         )
     `);
+
+    const result = await db.get(
+        "SELECT COUNT(*) as count FROM tasks"
+    );
+    /* Add sample tasks only if the table is empty,
+     so there is data available when testing the GET routes. */
+    if (result.count === 0){   
+        await db.run(
+            "INSERT INTO tasks(title,description) VALUES(?,?)",
+            ["Clean my room","Organize the messy clothes and dust off my gadgets."]
+        );
+        await db.run(
+            "INSERT INTO tasks(title,description) VALUES(?,?)",
+            ["Bake cookies", "Start baking cookies at 8pm following that new recipe i found."]
+        );
+    }
     return db;
 }
 module.exports = connectDatabase;
