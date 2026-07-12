19efd552cfd3ac3eefd266b4139dbd2d5eb2564d
MoemenSuper <ouerghuimoemej@gmail.com>
2026-07-12 10:04:37 +0100

    Add SQLite connection and create tasks table

--- config/database.js
+++ config/database.js
@@ -1,11 +1,21 @@
 const sqlite3 = require("sqlite3")
 const {open} = require("sqlite")
+const path = require("path");
 
 async function connectDatabase(){
     const db = await open({
-        filename: "database.sqlite",
+        filename: path.join(__dirname,"..","database.sqlite"),
         driver: sqlite3.Database
     });
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        done INTEGER DEFAULT 0,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
     return db;
 }
 module.exports = connectDatabase;
