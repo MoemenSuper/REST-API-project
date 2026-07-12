54d360185078833f46a1ba584a651facb4d4091d
MoemenSuper <ouerghuimoemej@gmail.com>
2026-07-12 11:12:24 +0100

    Added getTaskById and createTask model database functions

--- models/taskModel.js
+++ models/taskModel.js
@@ -2,11 +2,37 @@ const connectDB = require("../config/database");
 
 async function getAllTasks() {
     const db = await connectDB();
-    const tasks = await db.all("SELECT * from tasks");
+    const tasks = await db.all("SELECT * FROM tasks");
 
     return tasks;
 }
 
+async function getTaskById(id) {
+    const db = await connectDB();
+
+    const task = await db.get(
+        "SELECT * FROM tasks WHERE id = ?",
+        [id]
+    );
+
+    return task;
+}
+
+async function createTask(title, description) {
+    const db = await connectDB();
+
+    const result = await db.run(
+        "INSERT INTO tasks (title, description) VALUES (?,?)",
+        [title, description]
+    );
+
+    const newTask = await getTaskById(result.lastID);
+
+    return newTask;
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    getTaskById,
+    createTask
 };
